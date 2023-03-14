e21ca7c1ce82e6bd4c4527ba908b8f7235ad79f2
SivaKumar <sivakumar.g@improve10x.com>
2023-03-14 11:42:25 +0530

    i created photos activity

---
+++ app/src/main/java/com/improve10x/examapllication/PhotosApplicationActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.examapllication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhotosApplicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photos_application);
+    }
+}
